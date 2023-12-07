aa87e7e468617a25f31da97c6fd18b1c3c77fc76
nescpro <salazarnestor1996@gmail.com>
2023-12-07 15:40:25 -0500

    Create the orders app and the models: Order and OrderItem

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     # My Apss
     "store.apps.StoreConfig",
     "cart.apps.CartConfig",
+    "orders.apps.OrdersConfig",
     # Built-in
     "django.contrib.admin",
     "django.contrib.auth",
